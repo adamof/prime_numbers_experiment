4e8ffcd8126039c635dd843deff8344d9abddffd
Stefan Adamov <stefan.adamof@gmail.com>
2014-10-05 22:43:28 +0300

    Naive solution

--- challenge.rb
+++ challenge.rb
@@ -1,3 +1,30 @@
 class Challenge
+  def numbers(needed_count: 10)
+    numbers = [2,3]
 
+    number = 5
+
+    while numbers.length < needed_count
+      divider = get_divider(number)
+      numbers << number if divider == number
+      number += 2
+    end
+
+    numbers
+  end
+
+  private
+
+  def get_divider(number)
+    j = 2
+    while j <= number
+      break if number%j == 0
+      j += 1
+    end
+    j
+  end
+
+  def is_prime?(number)
+
+  end
 end
--- challenge_spec.rb
+++ challenge_spec.rb
@@ -4,4 +4,10 @@ describe Challenge do
   it 'should pass' do
     true
   end
+
+  describe '#numbers' do
+    it 'should return the first 10 prime numbers' do
+      expect(subject.numbers).to eql([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+    end
+  end
 end
