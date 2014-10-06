b937ca3bc05f45ad092bf2958ff94aee598431cc
Stefan Adamov <stefan.adamof@gmail.com>
2014-10-06 13:08:02 +0300

    Adds more specs

--- challenge.rb
+++ challenge.rb
@@ -1,25 +1,24 @@
 class Challenge
-  def initialize
-    @prime_numbers = [2]
-  end
+  def prime_numbers(needed_count: 10)
+    return [] unless needed_count > 0
 
-  def numbers(needed_count: 10)
+    prime_numbers = [2]
     number = 3
 
-    while @prime_numbers.length < needed_count
-      @prime_numbers << number if is_prime?(number)
+    while prime_numbers.length < needed_count
+      prime_numbers << number if is_prime?(number, prime_numbers)
       number += 2
     end
 
-    @prime_numbers
+    prime_numbers
   end
 
   private
 
-  def is_prime?(candidate_number)
+  def is_prime?(candidate_number, prime_numbers)
     sqrt_candidate_number = candidate_number**0.5
 
-    @prime_numbers.each do |prime_number|
+    prime_numbers.each do |prime_number|
       return true if prime_number > sqrt_candidate_number
       return false if candidate_number%prime_number == 0
     end
--- challenge_spec.rb
+++ challenge_spec.rb
@@ -1,13 +1,23 @@
 require './challenge'
 
 describe Challenge do
-  it 'should pass' do
-    true
-  end
+  describe '#prime_numbers' do
+    it 'should return the first 10 prime numbers, if no count is passed' do
+      expect(subject.prime_numbers).to eql(
+        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+      )
+    end
 
-  describe '#numbers' do
-    it 'should return the first 10 prime numbers' do
-      expect(subject.numbers).to eql([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+    it 'should return the first 5 prime numbers' do
+      expect(subject.prime_numbers(needed_count: 5)).to eql(
+        [2, 3, 5, 7, 11]
+      )
+    end
+
+    it 'should return [] if 0 length is passed' do
+      expect(subject.prime_numbers(needed_count: 0)).to eql(
+        []
+      )
     end
   end
 end
