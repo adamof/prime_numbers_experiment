fe747c3669b9d8c00a495565eb0dfe6bb4e33819
Stefan Adamov <stefan.adamof@gmail.com>
2014-10-06 13:01:46 +0300

    Optimisation - checks factors of primes

--- challenge.rb
+++ challenge.rb
@@ -1,30 +1,27 @@
 class Challenge
+  def initialize
+    @prime_numbers = [2]
+  end
+
   def numbers(needed_count: 10)
-    numbers = [2,3]
+    number = 3
 
-    number = 5
-
-    while numbers.length < needed_count
-      divider = get_divider(number)
-      numbers << number if divider == number
+    while @prime_numbers.length < needed_count
+      @prime_numbers << number if is_prime?(number)
       number += 2
     end
 
-    numbers
+    @prime_numbers
   end
 
   private
 
-  def get_divider(number)
-    j = 2
-    while j <= number
-      break if number%j == 0
-      j += 1
+  def is_prime?(candidate_number)
+    sqrt_candidate_number = candidate_number**0.5
+
+    @prime_numbers.each do |prime_number|
+      return true if prime_number > sqrt_candidate_number
+      return false if candidate_number%prime_number == 0
     end
-    j
-  end
-
-  def is_prime?(number)
-
   end
 end
